487148487ae57e3e885b8e903a74839dc0f82dba
Hila Ben Ami <hilab@post.bgu.ac.il>
2021-12-11 11:27:33 +0200

    find optimal lag to each two cells

--- granger_causality.py
+++ granger_causality.py
@@ -1,10 +1,16 @@
 import warnings
 from pathlib import Path
 
+from copy import deepcopy
 import numpy as np
 import pandas as pd
 from statsmodels.tsa.api import VAR
 from statsmodels.tsa.stattools import adfuller, kpss, grangercausalitytests
+
+from itertools import permutations
+import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
 
 # import logging
 # logger = logging.getLogger('my_module_name')
@@ -20,7 +26,7 @@ def load_file(path):
 
 
 def var_test(df):
-    opt_lag_dict = {'opt_lag': [], 'min_aic': []}
+    opt_lag_list = []
     for cell_id in df['cell_id'].unique():
         # print(f"** start test stationary - cell_id: {cell_id} **")
         cell_trajectory = df[df['cell_id'] == cell_id].copy().set_index('frame_no').drop(columns=['cell_id'])
@@ -29,7 +35,10 @@ def var_test(df):
         cell_trajectory = cell_trajectory.drop(columns=['x', 'y'])
         cell_trajectory = cell_trajectory.dropna()
         model = VAR(cell_trajectory)
-        # order_results = model.select_order(MAXLAG)
+        lags_results = model.select_order(MAXLAG)
+        lags = [lag_results.aic, lag_results.bic]
+        opt_lag_list.append(lags[np.argmin(lags)])
+        return
         # # print(order_results.ics)
         # print(order_results.selected_orders)
         # results = model.fit(MAXLAGs=order_results.selected_orders['aic'], ic='aic')
@@ -78,131 +87,107 @@ def adf_test(feature_series):
 
 
 def stationary_test(df, features):
-    stationary_results_adf = {}
+    stationary_results_combo = {}
     for col in features:
-        stationary_results_adf[col] = []
-    stationary_results_kpss = stationary_results_adf.copy()
+        stationary_results_combo[col] = []
+    stationary_results_combo['total'] = len(df['cell_id'].unique()) * [True]
     for cell_id in df['cell_id'].unique():
         # print(f"** start test stationary - cell_id: {cell_id} **")
         cell_trajectory = df[df['cell_id'] == cell_id].copy().reset_index(drop=True)
         cell_trajectory['dx'] = cell_trajectory['x'].diff()
         cell_trajectory['dy'] = cell_trajectory['y'].diff()
         cell_trajectory = cell_trajectory.drop(columns=['x', 'y'])
-        cell_trajectory = cell_trajectory.dropna()
+        cell_trajectory = cell_trajectory.dropna().reset_index(drop=True)
         for col in features:
             p_value_adf = adf_test(cell_trajectory[col])
             p_value_kpss = kpss_test(cell_trajectory[col])
-            stationary_results_adf[col].append(True if p_value_adf < 0.1 else False)
-            stationary_results_kpss[col].append(False if p_value_kpss < 0.1 else True)
-
-    avg_stat_dict = {'adf': {}, 'kpss': {}}
-    for col in features:
-        adf_avg_stat = np.mean(stationary_results_adf[col])
-        kpss_avg_stat = np.mean(stationary_results_kpss[col])
-        avg_stat_dict['adf'][col] = adf_avg_stat
-        avg_stat_dict['kpss'][col] = kpss_avg_stat
-        print(f"ADF stationary results: {adf_avg_stat} of the cells are stat in their {col}")
-        print(f"KPSS stationary results: {kpss_avg_stat} of the cells are stat in their {col}")
-    return avg_stat_dict
+            stationary_results_combo[col].append(True if p_value_adf <= 0.05 and p_value_kpss >= 0.05 else False)
+            stationary_results_combo['total'] = stationary_results_combo['total'] and \
+                                                stationary_results_combo[col]
+    stat_indexes = np.where(stationary_results_combo['total'])[0]
+    return stat_indexes
 
 
-def granger_causality_matrix(data, features, variables, res_df, test='ssr_chi2test', maxlag=MAXLAG, verbose=False, path=None):
+def granger_causality_matrix_2(data, features, res_df, res_col, test='ssr_chi2test', maxlag=MAXLAG, verbose=False):
     """
         The row are the response (y) and the columns are the predictors (x)
         If a given p-value is < significance level (0.05), we can reject the null hypothesis and conclude that walmart_x Granger causes apple_y.
     """
-    gc_per_feature_list = []
     for col in features:
         print(f"** Start calculate GC on {col} **")
+        res_df[col].loc[:, int(res_col)] = np.nan
         time_series_per_cell = {}
         feature_data = data[['frame_no', 'cell_id', col]].copy()
         for cell_id in feature_data['cell_id'].unique():
             time_series_per_cell[cell_id] = feature_data[feature_data['cell_id'] == cell_id].set_index('frame_no')[
                 col].diff()[1:]
         time_frame_df = pd.DataFrame(time_series_per_cell)
-        gc_df = pd.DataFrame(np.zeros((len(variables), len(variables))), columns=variables, index=variables)
-        for c in gc_df.columns:
-            for r in gc_df.index:
-                test_result = grangercausalitytests(time_frame_df[[r, c]], maxlag=maxlag, verbose=verbose)
-                p_values = [round(test_result[i + 1][0][test][1], 4) for i in range(maxlag)]
-                if verbose: print(f'Y = {r}, X = {c}, P Values = {p_values}')
-                min_p_value = np.min(p_values)
-                min_p_value_lag = np.argmin(p_values)
-                gc_df.loc[r, c] = min_p_value
-        gc_df.columns = [str(var) + '_x' for var in variables]
-        gc_df.index = [str(var) + '_y' for var in variables]
-        gc_per_feature_list.append(gc_df)
-        if path:
-            gc_df.to_csv(f'{path}/gc_{str(maxlag)}_{col}.csv')
-            gc_df = gc_df[gc_df < 0.05]
-            gc_df.to_csv(f'{path}/significant_gc_{str(maxlag)}_{col}.csv')
-    return gc_per_feature_list
+        for p, r in res_df[col].index:
+            pair_cells = time_frame_df[[r, p]]
+
+            model = VAR(pair_cells)
+            lags_results = model.select_order(MAXLAG)
+            lags = [lags_results.aic, lags_results.bic]
+            opt_lag = np.max([np.min(lags), 1])
+            gc_result = grangercausalitytests(time_frame_df[[r, p]], maxlag=opt_lag, verbose=verbose)
+            p_value = gc_result[opt_lag][0][test][1]
+            res_df[col].loc[(p, r), res_col] = p_value
+    return res_df
 
 
-def run_all_process(paths_list=None, folder=''):
-    global all_simulations
-    all_simulations = {'dist': [], 'dist_diff': [], 'ma_dist_diff': [], 'theta_dist': [], 'theta_dist_diff': [],
-                       'direction_diff': []}
+def run_pre_tests(df):
+    col_test_list = ['dx', 'dy']
+    stationary_passed_indexes = stationary_test(df, features=col_test_list)
+    return stationary_passed_indexes
+
+
+def run(paths_list=None, top_folder='', all_process=True):
+    all_simulation_gc_df = {}
     if paths_list is None:
         root_path = 'C:/Users/hilon/OneDrive - post.bgu.ac.il/תואר שני/master/vicsek-v2/examples'
-        folder = Path(root_path) / folder
-        paths_list = folder.rglob('frames_dfs*.csv')
+        files_path = Path(root_path) / top_folder
+        paths_list = files_path.rglob('frames_dfs*.csv')
     for simulation_no, path in enumerate(paths_list):
-        file_ts = path.name
-        print(f'start handling file no. {simulation_no}: {file_ts}')
-        df = load_file(path)
-        df = df.drop(columns=['heading'])
-        stationary_results = stationary_test(df, features=['dx', 'dy'])
-        col_test_list = ['dx', 'dy']
-        for col_test in col_test_list:
-            if stationary_results['adf'][col_test] > 0.7:
-                print(f"passed ADF stationary test on {col_test} feature")
-            if stationary_results['kpss'][col_test] > 0.7:
-                print(f"passed KPSS stationary test on {col_test} feature")
-        opt_lag = var_test(df)
-        granger_causality_matrix(df, features=set(df.columns) - set(['frame_no', 'cell_id']),
-                                 variables=df['cell_id'].unique(), path=path.parent)
-
-
-def run_only_gc(paths_list=None, folder=''):
-    all_simulation_gc_df = pd.DataFrame()
-    if paths_list is None:
-        root_path = 'C:/Users/hilon/OneDrive - post.bgu.ac.il/תואר שני/master/vicsek-v2/examples'
-        folder = Path(root_path) / folder
-        paths_list = folder.rglob('frames_dfs*.csv')
-    for simulation_no, path in enumerate(paths_list):
-        file_ts = path.name
-        print(f'start handling file no. {simulation_no}: {folder}/{file_ts}')
+        filename = path.name
+        folder = path.parent.parts[-1]
+        print(f'start handling file no. {simulation_no}: {folder}/{filename}')
         weight_param = folder[folder.rindex('_') + 1:]
-        all_simulation_gc_df[weight_param] = np.nan
         df = load_file(path)
         df = df.drop(columns=['heading'])
-        granger_causality_matrix(df, features=set(df.columns) - set(['frame_no', 'cell_id']),
-                                 variables=df['cell_id'].unique(), path=path.parent, maxlag=10, res_df=all_simulation_gc_df)
-
-def run(paths_list=None, folder='', all_process=True):
-    all_simulation_gc_df = pd.DataFrame()
-    if paths_list is None:
-        root_path = 'C:/Users/hilon/OneDrive - post.bgu.ac.il/תואר שני/master/vicsek-v2/examples'
-        folder = Path(root_path) / folder
-        paths_list = folder.rglob('frames_dfs*.csv')
-    for simulation_no, path in enumerate(paths_list):
-        file_ts = path.name
-        print(f'start handling file no. {simulation_no}: {folder}/{file_ts}')
-        weight_param = folder[folder.rindex('_') + 1:]
-        all_simulation_gc_df[weight_param] = np.nan
-        df = load_file(path)
-        df = df.drop(columns=['heading'])
+        df['cell_id'] = df['cell_id'].astype(str)
+        cell_ids = df['cell_id'].unique()
+        if simulation_no == 0:
+            gc_df = pd.DataFrame(np.zeros((len(cell_ids) * len(cell_ids) - len(cell_ids))), columns=['temp'])
+            gc_df['cell_predict'], gc_df['cell_response'] = zip(*permutations(cell_ids, 2))
+            gc_df.set_index(['cell_predict', 'cell_response'], inplace=True)
+            gc_df.drop(columns=['temp'], inplace=True)
+            for col in set(df.columns) - set(['frame_no', 'cell_id']):
+                all_simulation_gc_df[col] = gc_df.copy()
         if all_process:
-            run_all_process(paths_list=None, folder='')
-        else:
-            granger_causality_matrix(df, features=set(df.columns) - set(['frame_no', 'cell_id']),
-                                     variables=df['cell_id'].unique(), path=path.parent, maxlag=10,
-                                     res_df=all_simulation_gc_df)
+            stationary_passed_indexes = run_pre_tests(df)  ## todo filter un-stationary cells
+        granger_causality_matrix_2(df, features=set(df.columns) - set(['frame_no', 'cell_id']), maxlag=10,
+                                   res_df=all_simulation_gc_df, res_col=weight_param)
+    for col, df in all_simulation_gc_df.items():
+        df.to_csv(f'{files_path}/gc_{col}.csv')
+
+        fig, ax = plt.subplots(figsize=(70, 90))  # Sample figsize in inches
+        sns.heatmap(df, annot=True, ax=ax, linewidths=2)
+
+        ax.tick_params(axis='y', size=15, rotation=0, labelsize=40)
+        ax.tick_params(axis='x', size=15, labelsize=40)
+        plt.rc('font', size=30)  # controls default text sizes
+        plt.rc('legend', fontsize=30)  # legend fontsize
+        sns.heatmap(df, annot=True, ax=ax)
+        plt.savefig(f'{files_path}/gc_{col}.jpg')
 
 
 if __name__ == '__main__':
+    # path = 'C:\\Users\\hilon\\OneDrive - post.bgu.ac.il\\תואר שני\\master\\vicsek-v2\\examples\\081221'
+    # name = 'gc_x'
+    # df = pd.read_csv(f'{path}/{name}.csv', index_col=[0, 1])
+
     # run(folder='011221/04-12-21_1302')
     # run_only_gc(folder='011221/04-12-21_1304')
     # run_only_gc(folder='301121/30-11-21_1718')
-    run_only_gc(folder='081221')
+    # run(top_folder='temp', all_process=True)
+    run(top_folder='081221', all_process=True)
